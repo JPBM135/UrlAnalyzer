2085911dc8ec5ceccf42626ff1b1fa2d541f0620
João Pedro Borges Martins <jpedrobm0@gmail.com>
2023-02-03 04:11:07 -0300

    chore(client): move to src

--- packages/client/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { AnalyzerBar } from '@app/components/AnalyzerBar';
-
-export default function Home() {
-	return (
-		<main className="align-middle items-center pt-[3rem]">
-			<AnalyzerBar />
-		</main>
-	);
-}
--- packages/client/functions/makeApiReq.ts
+++
@@ -1,5 +0,0 @@
-export async function makeApiRequest<T>(path: string, options: RequestInit = {}) {
-	const res = await fetch(`http://localhost:3232/api/v1${path}`, options);
-
-	return res.json() as Promise<T>;
-}
