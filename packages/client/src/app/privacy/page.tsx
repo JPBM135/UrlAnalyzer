13b77c3e29d8372464ec043f5d366dae7a56aba7
João Pedro Borges Martins <jpedrobm0@gmail.com>
2023-02-03 04:54:50 -0300

    feat(pages): wip pages for terms and privacy policy

---
+++ packages/client/src/app/privacy/page.tsx
@@ -0,0 +1,7 @@
+export default function PrivacyPage() {
+	return (
+		<main className="flex flex-col justify-evenly p-64">
+			<div className="text-7xl font-bold text-center font-sans text-white pb-[7rem]">Feel the privacy (WIP)</div>
+		</main>
+	);
+}
---
+++ packages/client/src/app/terms/page.tsx
@@ -0,0 +1,7 @@
+export default function TermsPage() {
+	return (
+		<main className="flex flex-col justify-evenly p-64">
+			<div className="text-7xl font-bold text-center font-sans text-white pb-[7rem]">Feel the terms (WIP)</div>
+		</main>
+	);
+}
