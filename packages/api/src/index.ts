b85996c2f71c406a9aebc947f1c75eefe2c04b0c
João Pedro Borges Martins <jpedrobm0@gmail.com>
2023-02-08 05:44:44 -0300

    feat(api): add container functions

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,8 +1,11 @@
 import 'reflect-metadata';
-import { createPromRegistry } from 'container/prometheus.js';
+import { createCache } from 'container/cache.js';
 import { createRouter } from 'routes/index.js';
 import { createExpressApp } from './container/express.js';
+import { createImgurClient } from './container/imgur.js';
 import { createPostgres } from './container/postgres.js';
+import { createPromRegistry } from './container/prometheus.js';
+import { createPuppeteerBrowser } from './container/puppeteer.js';
 import { createRedis } from './container/redis.js';
 import logger from './logger.js';
 
@@ -17,6 +20,15 @@ logger.success('Redis client created');
 
 createPostgres();
 logger.success('Postgres client created');
+
+createImgurClient();
+logger.success('Imgur client created');
+
+createCache();
+logger.success('Cache created');
+
+await createPuppeteerBrowser();
+logger.success('Puppeteer browser created');
 
 await createRouter();
 logger.success('Routes created');
