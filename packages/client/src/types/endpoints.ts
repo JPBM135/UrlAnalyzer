e2d1f77c6f72a3ee9a2163ec86b2ee0e9d4aede5
João Pedro Borges Martins <jpedrobm0@gmail.com>
2023-02-10 03:41:23 -0300

    chore(types): createScan url

--- packages/client/src/types/endpoints.ts
+++ packages/client/src/types/endpoints.ts
@@ -45,6 +45,7 @@ export interface POSTAuthLoginEndpointBody {
 
 export type POSTScanResultEndpointReturn = GeneralEndpointReturn<{
 	id: string;
+	url: string;
 }>;
 export interface POSTScanResultEndpointBody {
 	url: string;
