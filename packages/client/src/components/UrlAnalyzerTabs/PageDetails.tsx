4929b1f1931a31600b2e7c8fd269a91e40548f70
João Pedro Borges Martins <jpedrobm0@gmail.com>
2023-02-11 14:04:22 -0300

    chore(console): color by message type

--- packages/client/src/components/UrlAnalyzerTabs/PageDetails.tsx
+++ packages/client/src/components/UrlAnalyzerTabs/PageDetails.tsx
@@ -2,8 +2,24 @@ import type { UrlAnalysisResult } from '@app/types/types';
 import type { FormatBodyReturn } from '@app/utils/formatBody';
 import { formatBody } from '@app/utils/formatBody';
 import { capitalizeFirstLetter } from '@app/utils/string';
+import type { ConsoleMessageType } from 'puppeteer';
 import { useEffect, useRef, useState } from 'react';
 import { CodeRenderer } from '../CodeRenderer';
+
+function colorFromConsoleType(type: ConsoleMessageType) {
+	switch (type) {
+		case 'error':
+			return 'red-400';
+		case 'info':
+			return 'blue-400';
+		case 'log':
+			return 'gray-400';
+		case 'warning':
+			return 'yellow-400';
+		default:
+			return 'white';
+	}
+}
 
 export function PageInfo({ result }: { result: UrlAnalysisResult }) {
 	const { consoleOutput, body } = result;
@@ -29,11 +45,17 @@ export function PageInfo({ result }: { result: UrlAnalysisResult }) {
 				<div className="text-3xl font-bold bg-gray-600 p-4 rounded">Console Output</div>
 				{consoleOutput.length ? (
 					consoleOutput.map((item, idx) => (
-						<div className="flex flex-col bg-gray-400 rounded p-4 text-xl gap-2" key={idx}>
-							<div className="flex">
-								<div className="font-bold">{capitalizeFirstLetter(item.type)}</div> <div>-</div> <div>{item.text}</div>
+						<div className="flex flex-col bg-gray-800 rounded p-4 text-xl gap-2" key={idx}>
+							<div className="flex gap-2 text-start">
+								<div className={`font-bold px-2 text-${colorFromConsoleType(item.type)}`}>
+									{capitalizeFirstLetter(item.type)}
+								</div>
+								- <span className="break-all">{item.text}</span>
 							</div>
-							{item.args.length && <div className="flex flex-col gap-2">{item.args.join(', ')}</div>}
+							<div className="flex gap-2">
+								<div className="font-bold px-2">Arguments:</div>
+								{item.args.length ? JSON.stringify(item.args) : 'No arguments'}
+							</div>
 						</div>
 					))
 				) : (
