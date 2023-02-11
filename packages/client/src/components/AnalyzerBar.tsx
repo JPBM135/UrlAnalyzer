686c42605a332622ed31ce1e9a3c4b49c02cd8e6
João Pedro Borges Martins <jpedrobm0@gmail.com>
2023-02-11 00:40:54 -0300

    chore(analyzerBar): style

--- packages/client/src/components/AnalyzerBar.tsx
+++ packages/client/src/components/AnalyzerBar.tsx
@@ -34,7 +34,10 @@ export function AnalyzerBar({ router }: { router: AppRouterInstance }) {
 	return (
 		<div className="flex flex-col justify-center items-center">
 			<div className="text-7xl font-bold text-white">URL Analyzer</div>
-			<form className="mt-10 p-4 rounded flex items-center bg-gray-200" onSubmit={async (elm) => onSubmit(elm, router)}>
+			<form
+				className="mt-10 p-4 rounded flex items-center bg-gray-200 justify-between gap-3"
+				onSubmit={async (elm) => onSubmit(elm, router)}
+			>
 				<input
 					className="w-96 h-10 p-2 rounded-lg border-2 border-gray-300 focus:outline-none focus:border-blue-300"
 					id="url-input"
