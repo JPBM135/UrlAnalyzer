c8991dd5ba71944174b278bc9c41ed2406a5b60e
João Pedro Borges Martins <jpedrobm0@gmail.com>
2023-02-11 14:05:06 -0300

    feat(generalInfo): metadata

--- packages/client/src/components/UrlAnalyzerTabs/GeneralInfo.tsx
+++ packages/client/src/components/UrlAnalyzerTabs/GeneralInfo.tsx
@@ -3,8 +3,10 @@ import { TransparencyReportGenericStatus, TransparencyReportFlags } from '@app/t
 import type { UrlAnalysisResult } from '@app/types/types';
 import { generateRandomHash } from '@app/utils/generateRandom';
 import { makeApiRequest } from '@app/utils/makeApiReq';
+import { capitalizeFirstLetter } from '@app/utils/string';
 import type { AppRouterInstance } from 'next/dist/shared/lib/app-router-context';
 import Image from 'next/image';
+import Link from 'next/link';
 import { useState } from 'react';
 import { FcHighPriority, FcOk } from 'react-icons/fc';
 import favicon from '../../../public/favicon.ico';
@@ -33,9 +35,11 @@ export function GeneralInfo({ result, router }: { result: UrlAnalysisResult; rou
 	const safeBrowsingIssue = Boolean(securityDetails.safeBrowsing.length);
 	const transparencyReportIssue = [2, 3, 5].includes(securityDetails.transparencyReport!.status!);
 
+	const metadata = Object.entries(result.metadata) as [string, string][];
+
 	return (
 		<div className="mb-10 mx-10 p-5 flex justify-between rounded-md bg-white items-start gap-7">
-			<div className="text-black text-xl">
+			<div className="text-black text-xl w-1/4">
 				<div className="bg-gray-300 rounded-md p-4 flex flex-col gap-4">
 					<div className="text-white bg-gray-500 p-2 rounded font-sans font-bold text-left text-2xl">General Info:</div>
 					<div>
@@ -121,7 +125,7 @@ export function GeneralInfo({ result, router }: { result: UrlAnalysisResult; rou
 						</details>
 					</div>
 				</div>
-				<div className="mt-4 w-fit">
+				<div className="mt-4">
 					<div className="bg-gray-300 rounded-md p-4">
 						<div className="text-white bg-gray-500 p-2 rounded font-sans font-bold text-left text-2xl">
 							SSL Certificate:
@@ -145,38 +149,66 @@ export function GeneralInfo({ result, router }: { result: UrlAnalysisResult; rou
 					</div>
 				</div>
 			</div>
-			<div className="bg-gray-300 rounded-md p-4">
-				<div className="text-white bg-gray-500 p-2 rounded font-sans font-bold text-left text-2xl">
-					Urls found: <div className="text-gray-200 text-sm">Do not click any urls that you don't trust!</div>
-				</div>
-				<div className="mt-3 flex flex-col gap-4">
-					{items.length ? (
-						items.map((url: string, idx: number) => (
-							<div
-								className={`flex justify-between items-center p-2 ${idx % 2 === 0 ? 'bg-gray-400' : 'bg-gray-100'}`}
-								key={generateRandomHash()}
-							>
-								<div>
-									<span className="text-xl font-bold text-gray-600 mr-2">{idx + 1} | Url: </span>{' '}
-									<span className="break-all">{url}</span>
-								</div>
+			<div className="flex flex-col w-1/2 gap-3">
+				<div className="bg-gray-300 rounded-md p-4 ">
+					<div className="text-white bg-gray-500 p-2 rounded font-sans font-bold text-left text-2xl">
+						Urls found: <div className="text-gray-200 text-sm">Do not click any urls that you don't trust!</div>
+					</div>
+					<div className="mt-3 flex flex-col gap-4">
+						{items.length ? (
+							items.map((url: string, idx: number) => (
 								<div
-									className="bg-gray-500 rounded-md py-1 px-2 ml-4 text-white cursor-pointer"
-									onClick={async () => onScanClick(router, url)}
+									className={`flex justify-between items-center rounded p-2 ${
+										idx % 2 === 0 ? 'bg-gray-400' : 'bg-gray-100'
+									}`}
+									key={generateRandomHash()}
 								>
-									Scan
+									<div>
+										<span className="text-xl font-bold text-gray-600 mr-2">{idx + 1} | Url: </span>{' '}
+										<span className="break-all">{url}</span>
+									</div>
+									<div
+										className="bg-gray-500 rounded-md py-1 px-2 ml-4 text-white cursor-pointer"
+										onClick={async () => onScanClick(router, url)}
+									>
+										Scan
+									</div>
 								</div>
-							</div>
-						))
-					) : (
-						<div className="text-xl font-bold text-gray-600">No urls found</div>
-					)}
-					{items.length ? <Pagination items={result.urlsFound} itemsPerPage={5} setItems={setItems} /> : null}
+							))
+						) : (
+							<div className="text-xl font-bold text-gray-600">No urls found</div>
+						)}
+						{items.length ? <Pagination items={result.urlsFound} itemsPerPage={5} setItems={setItems} /> : null}
+					</div>
+				</div>
+				<div className="flex flex-col bg-gray-300 rounded-md p-4 gap-4">
+					<div className="text-white bg-gray-500 p-2 rounded font-sans font-bold text-left text-2xl">Metadata:</div>
+					<div className="grid grid-cols-2 gap-2">
+						{metadata.map(([key, value]): JSX.Element => {
+							return (
+								<div className="flex flex-col break-words" key={generateRandomHash()}>
+									<div className="text-xl font-bold text-white bg-gray-700 p-1 rounded">
+										{capitalizeFirstLetter(key)}:
+									</div>
+									<div className="text-gray-600 text-xl">{value}</div>
+								</div>
+							);
+						})}
+					</div>
 				</div>
 			</div>
 			<div className="text-black text-xl w-[800px]">
 				<div className="bg-gray-300 rounded-md p-4 gap-3 flex flex-col">
-					<div className="text-white bg-gray-500 p-2 rounded font-sans font-bold text-left text-2xl">Screenshot:</div>
+					<div className="flex justify-between items-center text-white bg-gray-500 p-2 rounded font-sans font-bold text-left text-2xl">
+						Screenshot:
+						<Link
+							className="text-white bg-gray-800 p-2 rounded font-sans font-bold text-left text-2xl"
+							href={screenshot?.url ?? ''}
+							target="_blank"
+						>
+							See Full
+						</Link>
+					</div>
 					<Image
 						alt="Screenshot"
 						className="rounded"
