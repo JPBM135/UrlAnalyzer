d6a63a5fd9249b9fc4912430b72ee838dd9a17fc
João Pedro Borges Martins <jpedrobm0@gmail.com>
2023-04-25 03:02:30 -0300

    chore(respond): implement cache

--- packages/api/src/utils/respond.ts
+++ packages/api/src/utils/respond.ts
@@ -2,17 +2,29 @@ import type { HttpError } from '@structures/httpError.js';
 import type { Response } from 'express';
 import logger from '../logger.js';
 
-export function sendNonDataResponse(res: Response) {
+export enum CacheType {
+	Gigantic = 'public, max-age=1800, immutable',
+	Long = 'public, max-age=300, immutable',
+	NoStore = 'no-store',
+	Short = 'public, max-age=5, immutable',
+}
+
+export function sendNonDataResponse(res: Response, cache = CacheType.Short) {
 	try {
-		res.sendStatus(204).end();
+		res.set('Cache-Control', cache).sendStatus(204).end();
 	} catch (error) {
 		logger.error('Error sending non-data response: ' + (error as Error).message, error);
 	}
 }
 
-export function sendResponse<T extends { data: unknown; message: string }>(data: T['data'], res: Response) {
+export function sendResponse<T extends { data: unknown; message: string }>(
+	data: T['data'],
+	res: Response,
+	cache = CacheType.Short,
+) {
 	try {
 		res
+			.set('Cache-Control', cache)
 			.json({
 				message: 'success',
 				data,
@@ -24,9 +36,10 @@ export function sendResponse<T extends { data: unknown; message: string }>(data:
 	}
 }
 
-export function errorResponse(error: HttpError, res: Response) {
+export function errorResponse(error: HttpError, res: Response, cache = CacheType.NoStore) {
 	try {
 		res
+			.set('Cache-Control', cache)
 			.status(error.status)
 			.json({
 				message: 'error',
